feb755ed13edc32b74592af880ea9973a421b4d7
pennytv <157195845+pennytv@users.noreply.github.com>
2024-02-01 19:55:32 -0800

    Add files via upload

--- MadnessWithMethods.java
+++ MadnessWithMethods.java
@@ -10,16 +10,16 @@ public class MadnessWithMethods
 
     System.out.println(num1 + " is " + comparison + " than " + num2);
 }
-
+// Method to sum two integers
     private static int sumTwoInt(int num1, int num2)
     {return num1 + num2;
     }
-
+    //Method to get an integer from the user
     private static int getAnIntFromTheUser(Scanner scanner)
     {System.out.print("Please enter an integer: ");
         return scanner.nextInt();
     }
-
+    //Method to compare two integers
     private static String CompareTwoInt(int num1, int num2)
     {if (num1 > num2) {
         return "greater than";
